7ee0f9cc081a839045000f5d7e30634d5fc9c820
Brandon Macleod <BrandonMacleod21@gmail.com>
2020-06-03 20:49:46 +0000

    More refactoring with the input.

---
+++ input.js
@@ -0,0 +1,21 @@
+const handleUserInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.on('data', (key) => {
+    if (key === '\u0003') {
+      process.exit();
+    }
+  })
+};
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  handleUserInput();
+  return stdin;
+}
+
+module.exports = setupInput;
