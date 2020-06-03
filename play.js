089ff2644ea62f31656f61f044b315d95964d31a
Brandon Macleod <BrandonMacleod21@gmail.com>
2020-06-03 20:44:13 +0000

    Added ctrl + c as a way to exit.

--- play.js
+++ play.js
@@ -2,3 +2,27 @@ const { connect } = require('./client');
 
 console.log('Connecting ...');
 connect();
+
+const handleUserInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.on('data', (key) => {
+    if (key === '\u0003') {
+      process.exit();
+    }
+  })
+};
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  handleUserInput();
+  return stdin;
+}
+
+setupInput();
+
+
